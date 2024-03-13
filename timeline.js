6bf99fa60baaa223f8090c5abcff83f00c7bbda3
andreasvalen <37387676+andreasvalen@users.noreply.github.com>
2024-03-13 21:40:53 +0100

    Create timeline.js

---
+++ timeline.js
@@ -0,0 +1,12 @@
+BRYLLUPSDAG	
+08.00 	Hair and makeup moms (hotell i Ericeira)
+09.00 	Hair and makeup bride. (hotell i Ericeira)
+10.00 	Hair and make up forlovere. (hotell i Ericeira)
+11.00 -11.30	Frokost / brunsj     (hotell i Ericeira)
+12.00 	Kle på seg (hotell i Ericeira)
+12.45 (senest)	Transport Ericeira hotell til Quinta do Torneiro
+13.00	Pick up på hotellene i Ericeira (gjester)
+13.30	Ankomst lokale brud og forlovere
+13.30 - 14.00	Chille brud / fotograf
+14.00 - 15.00 	Guests arrive + Non-alcoholic beverages 
+15.00 - 15.30	Sermoni starter
